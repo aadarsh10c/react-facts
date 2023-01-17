9c13153abc2e320d64bdb57ed49828a1f2bd2b2e
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-17 21:23:55 +0530

    Created the boiler plate of main component

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 export default function Main () {
     return (
         <main className='Main'>
-            <h1>Main goes here</h1>
+            <ul className="main__list">
+                <h1 className="main__title">
+                    Fun facts about React
+                </h1>
+                <li className="main__list-points">Was first released in 2013</li>
+                <li className="main__list-points">Was originally created by Jordan Walke</li>
+                <li className="main__list-points">Has well over 100K stars on GitHub</li>
+                <li className="main__list-points">Is maintained by Facebook</li>
+                <li className="main__list-points">Powers thousands of enterprise apps, including mobile apps </li>
+            </ul>
         </main>
     );
 }
